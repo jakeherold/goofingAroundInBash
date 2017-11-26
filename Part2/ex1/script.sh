e1a9204081f55f96da9fdcbc80138cab40470364
Jake Herold <dcejake@gmail.com>
2017-11-26 12:54:07 -0800

    new exercise batch. Has script that takes in variable via command line, and prints it

---
+++ Part2/ex1/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Exercise 1
+
+echo Hello, who am I talking to?
+
+read varname
+
+echo It\'s nice to meet you, $varname
+
+# Synopsis: takes variable in via "read varname" which is then used as a variable and printed. #neat
