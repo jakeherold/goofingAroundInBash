3bb58ef13fd68e6476989f60b0f283f79821458e
Jake Herold <dcejake@gmail.com>
2017-11-26 12:49:19 -0800

    turns out I don't have the words directory I thought i did. :( I'll push on w/o completing this lesson, and research what that file should look like and see if i can manually recreate that kind of environment.

---
+++ Part1/exercise1/script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is the first assignment: Take command line variables and echo them back.
+
+echo "Echo AAAAAALLLL THE THINGS"
+echo $1
+echo $2
+echo $3
+echo
+echo "...and we're done echoing now."
